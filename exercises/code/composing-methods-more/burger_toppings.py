b544d41303f05404e53fac957658686f931320ab
Matthew Wei <matthewwei35@gmail.com>
2022-02-03 11:12:12 -0800

    feat(exercises): complete burger_toppings.py

---
+++ exercises/code/composing-methods-more/burger_toppings.py
@@ -0,0 +1,18 @@
+# by Kami Bigdely
+# Split temporary variable
+
+patty_weight = 70 # [gr]
+pickle_weight = 20 # [gr]
+tomato_weight = 25 # [gr]
+lettuce_weight = 15 # [gr]
+bun_weight = 95 # [gr]
+sandwich_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomato_weight + 2 * lettuce_weight
+                + 2 * bun_weight)
+print("NY Burger Weight", sandwich_weight)
+kimchi_weight = 30 # [gr]
+mayo_weight = 5 # [gr]
+golden_fried_onion_weight = 20 # [gr]
+sandwich_weight = (2 * patty_weight + 4 * pickle_weight + 3 * tomato_weight 
+                + kimchi_weight + mayo_weight + golden_fried_onion_weight + 2 * bun_weight)
+print("Seoul Kimchi Burger Weight", sandwich_weight)
+
