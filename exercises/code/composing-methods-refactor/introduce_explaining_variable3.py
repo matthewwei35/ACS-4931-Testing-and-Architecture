b6dccb6d7f9a3faab46e74465cfa645f3ce206b7
Matthew Wei <matthewwei35@gmail.com>
2022-02-03 10:35:08 -0800

    refactor(exercises): complete introduce_explaining_variable3.py

--- exercises/code/composing-methods-refactor/introduce_explaining_variable3.py
+++ exercises/code/composing-methods-refactor/introduce_explaining_variable3.py
@@ -5,11 +5,5 @@ MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
 def is_cooking_criteria_satisfied(time, temperature, pressure, desired_state):
-    is_desired_state = desired_state == 'well-done' or 'medium'
-    is_criteria_satisfied_well_done = time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE
-    is_criteria_satisfied_medium = time * temperature * pressure * COOKED_CONSTANT >= MEDIUM
-    if desired_state == is_desired_state and is_criteria_satisfied_well_done: 
-        return True
-    if desired_state == is_desired_state and is_criteria_satisfied_medium:
-        return True
-    return False
+    meat_criteria = time * temperature * pressure * COOKED_CONSTANT
+    return desired_state in ['well-done','medium'] and meat_criteria >= WELL_DONE or meat_criteria >= MEDIUM
